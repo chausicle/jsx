065cad09f147649ae826447888db15862a267b24
Justin Chau <chausicle@gmail.com>
2020-06-16 18:33:59 -0700

    Display simple label input and button

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
   return (
     <div>
-      App
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ background: "#e3e3e3", color: "grey" }}>Submit</button>
     </div>
   );
 };
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
